847c6476960afd7b8914f35bcd820bc71bc9d71d
Simon Sanderson <simon.j.sanderson@gmail.com>
2020-11-05 14:28:40 +1000

    Initial checkin

---
+++ esapi-building-footprints/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using IdentityModel.Client;
+
+namespace esapi_building_footprints
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /// Plans SDK Public account
+            const string ApplicationId = "app_822gf6lf9cvrmja8kj6j22t8s";
+            const string ApplicationSecret = "kK7Qk42GsG5NlQIdzw4nuRHK0";
+            const string ApiKey = "65101207-60c6-2250-3ec1-c99f3eb20793";
+            const string ApiSecret = "tlEQfN4PWfoJa0jNg/unshwf6SesDsF3rknNtwLYO+E=";
+
+            var bearerToken = $"bearer {GetBearerTokenEsapi(ApplicationId, ApplicationSecret, ApiKey, ApiSecret)}";
+
+            // EnumerateAllBuildings(bearerToken);
+            Console.WriteLine("Test");
+
+        }
+
+        public static string GetBearerTokenEsapi(string applicationId, string applicationSecret, string apiKey, string apiSecret)
+        {
+            var client = new HttpClient();
+            var ncswpf_passwordTokenRequest = new PasswordTokenRequest
+            {
+                Address = "https://auth.locatrix.com/connect/token",
+
+                ClientId = applicationId,
+                ClientSecret = applicationSecret,
+
+                Scope = "crmadapterservice-api-scope",
+
+                UserName = apiKey,
+                Password = apiSecret
+            };
+
+            var response = client.RequestPasswordTokenAsync(ncswpf_passwordTokenRequest).Result;
+            return response.AccessToken;
+        }
+    }
+}
---
+++ esapi-building-footprints/README.md
@@ -0,0 +1,17 @@
+# ESAPI Browser
+
+## Overview
+
+This is a simple starter application for the [Enterprise Services API](https://api.locatrix.com/esapi/api/docs/index.html) from [https://locatrix.com/](Locatrix) using the [Blazor WebAssembly](https://dotnet.microsoft.com/apps/aspnet/web-apps/blazor) framework in .NET Core 3.1.
+
+## Prerequisites
+
+- [https://dotnet.microsoft.com/download/dotnet-core/3.1]().NET Core 3.1.402) or higher.
+
+## Quick Start
+
+`dotnet run` and then open [https://localhost:5001](https://localhost:5001) in your browser.
+
+## Deep Dive
+
+TODO
